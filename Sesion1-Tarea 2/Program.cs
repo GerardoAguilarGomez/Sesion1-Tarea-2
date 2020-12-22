633df1e1c5ba4d690af6f8782d40d1a94397bfc5
Gerardo Aguilar Gomez <gerardo.aguilar@grupocastilla.es>
2020-12-22 13:33:55 +0100

    Agregar archivos de proyecto.

---
+++ Sesion1-Tarea 2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Sesion1_Tarea_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+
+            int num1 = 7;
+            int num2 = 8;
+            double num3 = 3.67;
+            double suma;
+            suma = num1 + num3;
+
+            Console.WriteLine("La suma es: " + suma);
+
+            string valor1 = Console.ReadLine();
+
+            Console.WriteLine("El número introducido es el " + valor1);
+
+            int valorint = Convert.ToInt32(valor1);
+
+            Console.ReadKey();
+
+        }
+    }
+}
